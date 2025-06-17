43e7c8de93a85c91f45f555713cf9c2e312feaad
tamaghnakundu <tkundu2004@gmail.com>
2025-06-18 00:01:44 +0530

    added main content and styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,14 @@
 export default function Main() {
-  return <h1>Main components go here</h1>;
+  return (
+    <>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200k stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </>
+  );
 }
